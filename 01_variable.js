7040485da24ac352783bc724d09015e27f43fa78
shivam patel <shivamsinghpatel4597@gmail.com>
2024-01-01 20:36:59 +0530

    changes in 01_variable file

--- 01_variable.js
+++ 01_variable.js
@@ -1,24 +1,27 @@
 // const accountId = 144556;
 // let accoundEmail = "hcq@gmail.com";
-let accountCity = "Rewa";
+var accountCity = "Rewa";
 let accountState;
 // var declare but not defined .
 //prints undefined
 
-/* dont use var beacause it changes everything var are fucntion scoped and let is block scoped 
-   see show1() and show2()
+/* 1. dont use var beacause it changes everything var are fucntion scoped and 
+let is block scoped see show1() and show2()
+   
+   2. if we declare var outside and changes its value inside function 
+   the value of var changes all over after that everywhere.
 */
 // console.log([accountId, accoundEmail, accountCity]);
 function show1() {
-  let accountCity = "satna";
+  // let accountCity = "satna";
   console.log(accountCity);
 }
 show1();
 
 function show2() {
-  // accountCity = "satna";
+  accountCity = "mangawan";
   console.log(accountCity);
 }
 show2();
 
-console.log(4 + 5);
+console.log(accountCity);
